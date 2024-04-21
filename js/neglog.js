1e0dc481268f5077eda87e2c0dc581492a84f3bd
AntoinePinardin <167693244+AntoinePinardin@users.noreply.github.com>
2024-04-21 17:01:59 +0100

    Update

---
+++ js/neglog.js
@@ -0,0 +1,6 @@
+function neglog(){
+	setTimeout(function(){document.getElementById("unilog").src="img/edine.png";},60);
+}
+function poslog(){
+	setTimeout(function(){document.getElementById("unilog").src="img/edi.png";},60);
+}
